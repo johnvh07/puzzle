1b15e2070294c6bc69c8d2dd4e329638efd946c9
kpa <admin@petervh.com>
2020-08-12 11:49:34 -0400

    When dropping a piece-group, shove covered pieces out of the way

--- client/script.js
+++ client/script.js
@@ -1,17 +1,20 @@
 /* global PIXI, _ */
 
-// Note: the image must be served via http* due to CORS.
+// Note: the image must be served via http* (not file://) due to CORS.
 
 // TODO:
-// + `grid.setSquarePos(squareID, col, row)`, `grid.getSquare(col, row)`, `grid.size`
-// + When picking up a piece, also pick up correct neighbors (and their correct neighbors, etc)
+// + Reactive-zoom the screen upon drop to always have 0.5 row/col empty on all sides (and allow dragging into that half row/col)
+//   + Should we change the x/y of every piece when this happens?  Or should we have 3 coordinate systems: SourceXY, StageXY, ScreenXY?
+//     + Rendering will use ScreenXY, clobbering will use StageXY, glueing will use SourceRowCol+StageRowCol, dragging will be a mess.
+// + On drop, save the state into localStorage.
+//   + Should serialization/deserialization be special functions?  Or should the entire state pass through an easily-serializable format, like cardtable does?
+//   + Add a dropdown-menu at the top-left with "Restart".
 
 // LATER:
+// + `grid.setSquarePos(squareID, col, row)`, `grid.getSquare(col, row)`, `grid.size`? with separate groundedGrid vs heldGrid?
 // + When dragging a group of pieces, show a shadow behind them (but above non-held pieces)
-// + Let users upload images (perhaps with password required)
 // + Rotate pieces - Tap an edge to point it up? Drag a corner (shown on hover)? Drag across rotator-zone? Right-click? Shake? Two-finger? Or don't?
-// + Add sync?
-// + Show a border between misaligned pieces?
+// + Show rectangles instead of squares to fill the screen?
 
 
 window._d = window._d || {}; // for debugging in browser console
@@ -28,11 +31,26 @@ function findGetParameter(parameterName) {
     });
   return result;
 }
+function extent(arr) {
+  let min, max;
+  min = max = arr[0];
+  for (let elem of arr) {
+    if (elem < min) { min = elem; }
+    if (elem > max) { max = elem; }
+  }
+  return [min, max];
+}
 
 const squareIDRowMultiplier = 10000; // Never do a puzzle with 10001 or more columns
 const getSquareID = function(row, column) {
   return row * squareIDRowMultiplier + column;
 };
+const getSourceRowCol = _.memoize(function(squareID) {
+  return [
+    Math.floor(squareID / squareIDRowMultiplier),
+    squareID % squareIDRowMultiplier
+  ];
+});
 
 const minNumPieces = +findGetParameter('pieces') || 100; console.log('Note: Try appending ?pieces=30 to URL');
 
@@ -40,6 +58,7 @@ const app = new PIXI.Application({
   backgroundColor:0x444444,
   width:window.innerWidth - 3, height:window.innerHeight - 10,
 });
+_d.app = app;
 app.stage.sortableChildren = true; // required for zIndex to have any effect
 document.body.appendChild(app.view);
 const imgPaths = _.range(1, 1+47).map(i => `https://petervh.com/viv-frames/${i}.jpg`);
@@ -89,15 +108,29 @@ app.loader.load(function() {
       }
     }
   }());
-  function getXYFromRowCol(row, col) {
+  function getScreenXYFromRowCol(row, col) {
     return [0.5 * screenSquareSize + col * screenSquareSize, 0.5 * screenSquareSize + row * screenSquareSize];
   }
-  const screenXYPairs = screenRowColPairs.map(([row,col]) => getXYFromRowCol(row, col));
-  function getRowColFromXY(x, y) {
+  const screenXYPairs = screenRowColPairs.map(([row,col]) => getScreenXYFromRowCol(row, col));
+  function getScreenRowColFromXY(x, y) {
     x = _.clamp(x, 0, screenNumCols * screenSquareSize - 0.01);
     y = _.clamp(y, 0, screenNumRows * screenSquareSize - 0.01);
     return [Math.floor(y / screenSquareSize), Math.floor(x / screenSquareSize)];
   }
+
+  const getNeighborIDs = _.memoize(function(squareID) {
+    // Given a squareID like `30004` (ie, row=3, column=4),
+    // return the IDs of the squares above, below, and beside the square,
+    // which are [20004, 30003, 30005, 40004].
+    squareID = +squareID;
+    const [row, col] = getSourceRowCol(squareID);
+    const ret = [];
+    if (row > 0) { ret.push(getSquareID(row-1, col)); }
+    if (col > 0) { ret.push(getSquareID(row, col-1)); }
+    if (row+1 < sourceNumRows) { ret.push(getSquareID(row+1, col)); }
+    if (col+1 < sourceNumCols) { ret.push(getSquareID(row, col+1)); }
+    return ret;
+  });
 
   // Initialize the squares:
   const squares = {}; window._d.squares = squares;
@@ -143,41 +176,113 @@ app.loader.load(function() {
     const square = this;
     const pointerPosition = event.data.getLocalPosition(square.parent);
     square.dragStartOffset = {
-      x: pointerPosition.x - this.position.x,
-      y: pointerPosition.y - this.position.y,
+      x: pointerPosition.x - square.position.x,
+      y: pointerPosition.y - square.position.y,
     };
     square.isDragging = true;
-    square.zIndex = 1; // Show this square above non-held squares.  This requires 'this.parent.sortableChildren'.
+
+    // Bring along aligned neighbors
+    square.aligned = {squares: []};
+    function addAlignedSquare(alignedSquareID) {
+      if (!square.aligned.squares.includes(alignedSquareID)) {
+        square.aligned.squares.push(alignedSquareID);
+        const alignedSquare = squares[alignedSquareID];
+        const alignedSourceRowCol = getSourceRowCol(alignedSquareID);
+        const alignedScreenRowCol = getScreenRowColFromXY(alignedSquare.x, alignedSquare.y);
+        getNeighborIDs(alignedSquareID).forEach(neighborID => {
+          if (!squares[neighborID]) {
+            console.error(`Tried to get fake squareID ${neighborID}`);
+          }
+          const neighborSquare = squares[neighborID];
+          const neighborSourceRowCol = getSourceRowCol(neighborID);
+          const neighborScreenRowCol = getScreenRowColFromXY(neighborSquare.x, neighborSquare.y);
+          const isAligned = (
+            (neighborSourceRowCol[0] - alignedSourceRowCol[0] === neighborScreenRowCol[0] - alignedScreenRowCol[0]) &&
+            (neighborSourceRowCol[1] - alignedSourceRowCol[1] === neighborScreenRowCol[1] - alignedScreenRowCol[1])
+          );
+          if (isAligned) { addAlignedSquare(neighborID); }
+        });
+      }
+    }
+    addAlignedSquare(square.squareID);
+
+    // Calculate colOffsetExtent and rowOffsetExtent to prevent dragging a group off the screen edge
+    const mySourceRowCol = getSourceRowCol(square.squareID);
+    square.aligned.colOffsetExtent = extent(square.aligned.squares.map(sqID => getSourceRowCol(sqID)[1] - mySourceRowCol[1]));
+    square.aligned.rowOffsetExtent = extent(square.aligned.squares.map(sqID => getSourceRowCol(sqID)[0] - mySourceRowCol[0]));
+
+    // Show held squares above non-held squares.  This requires 'this.parent.sortableChildren'.
+    square.aligned.squares.forEach(sqID => { squares[sqID].zIndex = 1; });
+
+    // console.log(mySourceRowCol, square.aligned);
   }
   function onDragMove(event) {
     const square = this;
-    if (square.isDragging) {
+    if (square.isDragging && square.aligned && square.aligned.squares.length) {
       const pointerPosition = event.data.getLocalPosition(square.parent);
-      var squarePosition = {
-        x: pointerPosition.x - this.dragStartOffset.x,
-        y: pointerPosition.y - this.dragStartOffset.y
-      };
-      [square.x, square.y] = getXYFromRowCol(...getRowColFromXY(squarePosition.x, squarePosition.y));
+      let squareXY = [
+        pointerPosition.x - square.dragStartOffset.x,
+        pointerPosition.y - square.dragStartOffset.y
+      ];
+
+      // If any squares in the group are off the screen, move myScreenRowCol until they're back in.
+      let myScreenRowCol = getScreenRowColFromXY(...squareXY);
+      const smallestCol = myScreenRowCol[1] + square.aligned.colOffsetExtent[0];
+      if (smallestCol < 0) { myScreenRowCol[1] += -smallestCol; }
+      const smallestRow = myScreenRowCol[0] + square.aligned.rowOffsetExtent[0];
+      if (smallestRow < 0 ) { myScreenRowCol[0] += -smallestRow; }
+      const biggestCol = myScreenRowCol[1] + square.aligned.colOffsetExtent[1];
+      if (biggestCol >= screenNumCols ) { myScreenRowCol[1] += screenNumCols - biggestCol - 1; }
+      const biggestRow = myScreenRowCol[0] + square.aligned.rowOffsetExtent[1];
+      if (biggestRow >= screenNumRows ) { myScreenRowCol[0] += screenNumRows - biggestRow - 1; }
+
+      squareXY = getScreenXYFromRowCol(...myScreenRowCol); // Use new rowCol and also align to grid
+
+      const mySourceRowCol = getSourceRowCol(square.squareID);
+      square.aligned.squares.forEach(sqID => {
+        const sq = squares[sqID];
+        const sqSourceRowCol = getSourceRowCol(sqID);
+        sq.x = squareXY[0] + screenSquareSize * (sqSourceRowCol[1] - mySourceRowCol[1]);
+        sq.y = squareXY[1] + screenSquareSize * (sqSourceRowCol[0] - mySourceRowCol[0]);
+      });
     }
   }
   function onDragEnd() {
     const square = this;
-    square.isDragging = false;
-    square.dragStartOffset = undefined;
-    square.zIndex = 0;
-    // Check if this piece covers any other piece
-    // TODO: Make this not be O(n^2).  Maybe make a set of occupied XY before iterating squareXYPairs.
-    _.each(squares, sq => {
-      if (sq.squareID !== square.squareID && Math.abs(sq.x - square.x) < screenSquareSize/4 && Math.abs(sq.y - square.y) < screenSquareSize/4) {
-        // Find an empty spot
-        for (let [x, y] of screenXYPairs.slice().reverse()) {
-          if (!_.some(squares, s => s.x === x && s.y === y)) {
-            sq.x = x;
-            sq.y = y;
-            break;
-          }
-        }
-      }
-    });
+    if (square.isDragging && square.aligned) {
+      square.isDragging = false;
+      square.dragStartOffset = undefined;
+
+      // Shove the squares getting covered-up into empty spaces
+      // TODO: instead of re-using getSquareID, make an equivalent function with a better name
+      const groundOccupiedPositions = _.filter(squares, sq => sq.zIndex === 0).map(sq => getSquareID(...getScreenRowColFromXY(sq.x, sq.y)));
+      const airOccupiedPositions = _.filter(squares, sq => sq.zIndex === 1).map(sq => getSquareID(...getScreenRowColFromXY(sq.x, sq.y)));
+      const clobberedPositions = _.intersection(groundOccupiedPositions, airOccupiedPositions);
+      const occupiedPositions = _.union(groundOccupiedPositions, airOccupiedPositions);
+      const allPositions = screenRowColPairs.map(([row, col]) => getSquareID(row, col));
+      const emptyPositions = _.difference(allPositions, occupiedPositions);
+      const clobberedSquares = _.filter(squares, sq => {
+        if (sq.zIndex !== 0) { return false; }
+        const screenRowCol = getScreenRowColFromXY(sq.x, sq.y);
+        const positionID = getSquareID(...screenRowCol);
+        return clobberedPositions.includes(positionID);
+      })
+      // console.log('groundOccupiedPositions =', _.sortBy(groundOccupiedPositions));
+      // console.log('airOccupiedPositions =', _.sortBy(airOccupiedPositions));
+      // console.log('clobberedPositions =', _.sortBy(clobberedPositions));
+      // console.log('clobberedSquares =', clobberedSquares);
+      // console.log('occupiedPositions =', _.sortBy(occupiedPositions));
+      // console.log('allPositions =', _.sortBy(allPositions));
+      // console.log('emptyPositions =', _.sortBy(emptyPositions));
+      clobberedSquares.forEach(sq => {
+        const newPosition = emptyPositions.pop();
+        const newXY = getScreenXYFromRowCol(...getSourceRowCol(newPosition));
+        // console.log(`moving clobbered sq from ${sq.x},${sq.y} -> ${newXY[0]},${newXY[1]}`);
+        [sq.x, sq.y] = newXY;
+      });
+
+      square.aligned.squares.forEach(sqID => { squares[sqID].zIndex = 0; });
+      delete square.aligned;
+    }
   }
 });
