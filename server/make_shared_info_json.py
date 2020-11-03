77c37bf258390464d4b8ef1ed4e24a26844da922
Peter VandeHaar <pjvandehaar@gmail.com>
2020-11-03 22:26:52 +0000

    .

--- server/make_shared_info_json.py
+++ server/make_shared_info_json.py
@@ -10,8 +10,6 @@ def make_shared_info_json():
         if dirpath.is_dir():
             j = json.loads((dirpath / 'info.json').read_text())
             j['name'] = dirpath.name
-            if '320px_mp4' in j: j['320px_mp4'] = f"https://petervh.com/live/{dirpath.name}/{j['320px_mp4']}"
-            if '320px_jpg' in j: j['320px_jpg'] = f"https://petervh.com/live/{dirpath.name}/{j['320px_jpg']}"
             ret.append(j)
     (serve_dir_path / 'info.json').write_text(json.dumps(ret))
 
